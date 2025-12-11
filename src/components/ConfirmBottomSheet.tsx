f5e645a9408e321ba57bfd793756ce05c475111f
sebanimm <gks32114221@gmail.com>
2025-12-11 17:17:44 +0900

    feat: update Confirm and Gender Bottom Sheets to use Drawer.Title and Drawer.Description components

--- src/components/ConfirmBottomSheet.tsx
+++ src/components/ConfirmBottomSheet.tsx
@@ -27,7 +27,7 @@ export const ConfirmBottomSheet = ({
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0" />
         <Drawer.Content className="fixed right-0 bottom-0 left-0 z-20 flex flex-col rounded-t-[36px] bg-[#171719]">
-          <div className="mx-auto flex h-[80dvh] w-full flex-col gap-12 px-20 pb-10">
+          <div className="font-pretendard mx-auto flex h-[80dvh] w-full flex-col gap-12 px-20 pb-10">
             {/* Handle Bar */}
             <div className="flex items-center justify-center px-2.5 py-8">
               <div className="h-2.5 w-[103px] rounded-full bg-[#AEB0B6]" />
@@ -35,12 +35,16 @@ export const ConfirmBottomSheet = ({
 
             {/* Title */}
             <div className="flex flex-col gap-3 py-10">
-              <h2 className="gradient-text text-[3.5rem] leading-[1.3] font-medium tracking-[-0.07rem]">
-                {t("confirm.restart.title")}
-              </h2>
-              <p className="text-[2.5rem] leading-[1.3] tracking-[-0.025rem] text-[#989BA2]">
-                {t("confirm.restart.description")}
-              </p>
+              <Drawer.Title asChild>
+                <h2 className="gradient-text text-[3.5rem] leading-[1.3] font-medium tracking-[-0.07rem]">
+                  {t("confirm.restart.title")}
+                </h2>
+              </Drawer.Title>
+              <Drawer.Description asChild>
+                <p className="text-[2.5rem] leading-[1.3] tracking-[-0.025rem] text-[#989BA2]">
+                  {t("confirm.restart.description")}
+                </p>
+              </Drawer.Description>
             </div>
 
             {/* Buttons */}
